869e7598e25f666535056495fcfb8e82718e90d9
Rafif <rafifzaidan07@gmail.com>
2026-07-03 12:35:47 +0700

    Add video_player_win dependency to resolve Windows playback unimplemented error

--- mobile_app/windows/flutter/generated_plugin_registrant.cc
+++ mobile_app/windows/flutter/generated_plugin_registrant.cc
@@ -11,6 +11,7 @@
 #include <file_selector_windows/file_selector_windows.h>
 #include <syncfusion_pdfviewer_windows/syncfusion_pdfviewer_windows_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
+#include <video_player_win/video_player_win_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AppLinksPluginCApiRegisterWithRegistrar(
@@ -23,4 +24,6 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("SyncfusionPdfviewerWindowsPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
+  VideoPlayerWinPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("VideoPlayerWinPluginCApi"));
 }
